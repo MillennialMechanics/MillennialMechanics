ebe0ff5004d1c8635e851cc60204dd8b85563f29
MillennialMechanics <47344403+MillennialMechanics@users.noreply.github.com>
2019-02-28 10:04:11 -0700

    Update Links.js

--- vehicles-gallery-master/src/pages/Car/Links/Links.js
+++ vehicles-gallery-master/src/pages/Car/Links/Links.js
@@ -11,10 +11,16 @@ const Links = ({ links }) => {
   return (
     <ul className="links">
       {links.map(link => {
-        const { text, link: href } = link;
+        const { text, link: href, content } = link;
         return (
           <li>
-            <a href={href}>{text}</a>
+            {content ? (
+              <div dangerouslySetInnerHTML={{ __html: content }} />
+            ) : (
+              <a href={href} target="_blank" rel="noopener noreferrer">
+                {text}
+              </a>
+            )}
           </li>
         );
       })}
